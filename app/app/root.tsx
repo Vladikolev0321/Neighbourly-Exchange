075a1930088fbe6f87c77579cde2258cf38b7a2e
Duj0 <denislazarov1@gmail.com>
2024-12-02 16:28:50 +0200

    added clerk

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -22,12 +22,24 @@ export const links: LinksFunction = () => [
   },
 ];
 
+import type { MetaFunction, LoaderFunction } from "@remix-run/node";
+
+// Import rootAuthLoader
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+
+export const meta: MetaFunction = () => ([{
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+}]);
+
+// Export as the root route loader
+export const loader: LoaderFunction = (args) => rootAuthLoader(args);
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
@@ -40,6 +52,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
+export  function App() {
   return <Outlet />;
 }
+
+import { ClerkApp } from "@clerk/remix";
+export default ClerkApp(App);
